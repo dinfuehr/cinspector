d116e1f960e2179c98b93468f4b0bac5ffa02e05
Dominik Inführ <dominik.infuehr@gmail.com>
2017-04-29 00:00:58 +0200

    add some actions

--- src/ui/components/top-header/component.ts
+++ src/ui/components/top-header/component.ts
@@ -1,11 +1,21 @@
-import Component from '@glimmer/component';
+import Component, { tracked } from '@glimmer/component';
 
 export default class TopHeader extends Component {
+    @tracked
+    counter: number;
+    
     user = {
         name: 'Dominik'
     };
 
+    constructor(options) {
+        super(options);
+
+        this.counter = 0;
+    }
+
     next() {
         console.log("clicked");
+        this.counter += 1;
     }
 };
